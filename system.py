77996350bd02652ac067a477378fd807fa65d753
TheBackman22 <53570198+TheBackman22@users.noreply.github.com>
2022-06-15 17:57:13 -0500

    Revert "simple diagnostic to message converter"
    This reverts commit 0873a67c736ec46a05a07ae6a53cb1a751fead9c.

--- system.py
+++ system.py
@@ -5,25 +5,25 @@ import psutil
 def get_system_info():
     sys = platform.platform()
     if sys[0:3] == "mac":
-        return get_mac_sys_info()
+        get_mac_sys_info()
     elif sys[0:3] == "Win":
-        return get_windows_sys_info()
+        get_windows_sys_info()
     else: print("err")
 
 def get_mac_sys_info():
     print("mac")
-    # print("cpu usage percentage: %s" %  psutil.cpu_percent())
+    print("cpu usage percentage: %s" %  psutil.cpu_percent())
     # print(psutil.cpu_stats())
     # print(psutil.cpu_freq())
-    return  psutil.cpu_percent()
+    return
 
 def get_windows_sys_info():
     print("win")
-    # print("cpu usage percentage: %s" %  psutil.cpu_percent())
-    return psutil.cpu_percent()
+    print("cpu usage percentage: %s" %  psutil.cpu_percent())
+    return
 
-# if __name__ == '__main__':
-# 	get_system_info()
+if __name__ == '__main__':
+	get_system_info()
 
 
 
