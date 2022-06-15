82d6cf71355f8d7e7da219bc30f3a9213b0dad04
spinto1 <70720130+spinto1@users.noreply.github.com>
2022-06-15 14:08:08 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("adaptPlayer")
+window.geometry("600x400")
+
+
+label = tk.Label(text = "Creating a test desktop app")
+label.grid(column=0, row=0)
+window.mainloop()
