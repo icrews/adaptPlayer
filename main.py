a1ae8d43e00f959f4fa5b39651f3dc0a24beed88
Isaiah Crews <icrews@iwu.edu>
2022-06-16 00:12:16 -0500

    Added Settings button which opens external Python document, also can embed html in embedder.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import tkinter
+from cefpython3 import cefpython as cef
 import customtkinter  # credit to Tom Schimansky
 from PIL import Image, ImageTk
 import os
+import sys
+import platform
+import logging as _logging
+import tkinter.filedialog
 
 # customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
 # customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -16,12 +21,28 @@ PATH = os.path.dirname(os.path.realpath(__file__))
 # def slider_callback(value):
 #     progressbar_1.set(value)
 
+# Fix for PyCharm hints warnings
+WindowUtils = cef.WindowUtils()
+
+# Platforms
+WINDOWS = (platform.system() == "Windows")
+LINUX = (platform.system() == "Linux")
+MAC = (platform.system() == "Darwin")
+
+# Globals
+logger = _logging.getLogger("tkinter_.py")
+
+# Constants
+# Tk 8.5 doesn't support png images
+IMAGE_EXT = ".png" if tkinter.TkVersion > 8.5 else ".gif"
+
 
 def button_function():
     print("Login pressed")
 
 
 class App(customtkinter.CTk):
+
 
     APP_NAME = "DaddyApp"
     WIDTH = 350
@@ -37,6 +58,7 @@ class App(customtkinter.CTk):
         refresh_image = ImageTk.PhotoImage(Image.open(PATH + "/images/rotate.png"))
         setting_image = ImageTk.PhotoImage(Image.open(PATH + "/images/setting.png"))
         close_image = ImageTk.PhotoImage(Image.open(PATH + "/images/close.png"))
+
 
         # Size Control
         self.title(App.APP_NAME)
@@ -59,6 +81,9 @@ class App(customtkinter.CTk):
         self.button = customtkinter.CTkButton(self, border_color="#D35B58", fg_color="#c0392b", hover_color="#e74c3c",
                                               image=refresh_image, text="", command=self.create_new_window)
         self.button.pack(side="top", padx=40, pady=40)
+        self.button_3 = customtkinter.CTkButton(self, border_color="#D35B58", fg_color="#c0392b", hover_color="#e74c3c",
+                                                image=setting_image, text="", command=self.open)
+        self.button_3.pack(side="bottom", padx=40, pady=40)
 
 
     def close_button(self):
@@ -71,6 +96,10 @@ class App(customtkinter.CTk):
         window = customtkinter.CTkToplevel(self)
         window.geometry("400x200")
         self.overrideredirect(True)
+
+    def open(self):
+        PathPy = tkinter.filedialog.askopenfilename(title="Open a file", filetypes=[('PYTHON file', '.py')])
+        os.system('%s %s' % (sys.executable, PathPy))
 
     def start(self):
         self.mainloop()
