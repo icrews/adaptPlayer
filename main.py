550664ddf39acb5669691943132bba27aaf0e50e
Isaiah Crews <icrews@iwu.edu>
2022-06-15 22:43:21 -0500

    Fixed frame close error

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ PATH = os.path.dirname(os.path.realpath(__file__))
 #     progressbar_1.set(value)
 
 
+def button_function():
+    print("Login pressed")
+
 
 class App(customtkinter.CTk):
 
     APP_NAME = "DaddyApp"
     WIDTH = 350
     HEIGHT = 1200
-
-    def close_button(self):
-        self.destroy()
 
     def __init__(self, *args, **kwargs):
         # Setup
@@ -46,38 +46,34 @@ class App(customtkinter.CTk):
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
         # self.attributes('-alpha', 0.5) # Transparent Window
         self.overrideredirect(True)
-        self.geometry("350x1200+100+100")
 
         image = Image.open(PATH + "/images/OratorBG.png").resize((self.WIDTH, self.HEIGHT))
         self.bg_image = ImageTk.PhotoImage(image)
         self.image_label = tkinter.Label(master=self, image=self.bg_image)
         self.image_label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
+
+        self.button_2 = customtkinter.CTkButton(self, border_color="#D35B58", fg_color="#c0392b", hover_color="#e74c3c",
+                                              image=close_image, text="", command=self.close_button)
+        self.button_2.pack(side="right", padx=40, pady=40)
         self.button = customtkinter.CTkButton(self, border_color="#D35B58", fg_color="#c0392b", hover_color="#e74c3c",
                                               image=refresh_image, text="", command=self.create_new_window)
         self.button.pack(side="top", padx=40, pady=40)
 
-        self.button = customtkinter.CTkButton(self, border_color="#D35B58", fg_color="#c0392b", hover_color="#e74c3c",
-                                              image=close_image, text="", command=self.close_button())
-        self.button.pack(side="right", padx=40, pady=40)
 
-
-
-    def button_function(self):
-        print("Login pressed")
+    def close_button(self):
+        self.destroy()
 
     def on_closing(self, event=0):
         self.destroy()
-
-    def start(self):
-        self.mainloop()
 
     def create_new_window(self):
         window = customtkinter.CTkToplevel(self)
         window.geometry("400x200")
         self.overrideredirect(True)
 
-
+    def start(self):
+        self.mainloop()
 
 
 if __name__ == "__main__":
@@ -85,46 +81,6 @@ if __name__ == "__main__":
     app.start()
 
 
-# frame_1 = customtkinter.CTkFrame(master=window)
-# frame_1.pack(pady=20, padx=60, fill="both", expand=True)
-#
-# label_1 = customtkinter.CTkLabel(master=frame_1, justify=tk.LEFT, text="Lady")
-# label_1.pack(pady=12, padx=10)
-#
-# progressbar_1 = customtkinter.CTkProgressBar(master=frame_1)
-# progressbar_1.pack(pady=12, padx=10)
-#
-# button_1 = customtkinter.CTkButton(master=frame_1, command=button_callback, text="Daddy")
-# button_1.pack(pady=12, padx=10)
-#
-# slider_1 = customtkinter.CTkSlider(master=frame_1, command=slider_callback, from_=0, to=1)
-# slider_1.pack(pady=12, padx=10)
-# slider_1.set(0.5)
-#
-# entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="CTkEntry")
-# entry_1.pack(pady=12, padx=10)
-#
-# optionmenu_1 = customtkinter.CTkOptionMenu(frame_1, values=["Option 1", "Option 2", "Option 42 long long long..."])
-# optionmenu_1.pack(pady=12, padx=10)
-# optionmenu_1.set("CTkOptionMenu")
-#
-# combobox_1 = customtkinter.CTkComboBox(frame_1, values=["Option 1", "Option 2", "Option 42 long long long..."])
-# combobox_1.pack(pady=12, padx=10)
-# combobox_1.set("CTkComboBox")
-#
-# checkbox_1 = customtkinter.CTkCheckBox(master=frame_1)
-# checkbox_1.pack(pady=12, padx=10)
-#
-# radiobutton_var = tk.IntVar(value=1)
-#
-# radiobutton_1 = customtkinter.CTkRadioButton(master=frame_1, variable=radiobutton_var, value=1)
-# radiobutton_1.pack(pady=12, padx=10)
-#
-# radiobutton_2 = customtkinter.CTkRadioButton(master=frame_1, variable=radiobutton_var, value=2)
-# radiobutton_2.pack(pady=12, padx=10)
-#
-# switch_1 = customtkinter.CTkSwitch(master=frame_1)
-# switch_1.pack(pady=12, padx=10)
 
 
 
